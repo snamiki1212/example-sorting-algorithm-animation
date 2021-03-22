42527b788d4fc4a2381d7272c88574dc0d2f2bfb
snamiki1212 <snamiki1212@gmail.com>
2021-03-22 15:10:06 -0700

    Refactor dir

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
+import { Home } from "../src/components/pages/Home";
 
-export default function Home() {
+export default function Index() {
   return (
     <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>this is home page</div>
+      <Home />
     </div>
   );
 }
---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,3 @@
+export function Home() {
+  return <div>this is Top</div>;
+}
