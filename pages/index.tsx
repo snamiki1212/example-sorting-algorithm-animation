9c034c55a69a3afe75aa52c22480b7cb473896a5
snamiki1212 <snamiki1212@gmail.com>
2021-03-22 15:03:14 -0700

    Upgrade typescript

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>this is home page</div>
+    </div>
+  );
+}
