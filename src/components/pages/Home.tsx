d878199ebefceff256753a912d47024ded50009a
snamiki1212 <snamiki1212@gmail.com>
2021-03-23 12:38:32 -0700

    Update

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,6 +3,12 @@ import { motion, useMotionValue, useTransform } from "framer-motion";
 
 const ITEMS = Array.from({ length: 10 }, (_, idx) => idx);
 
+const spring = {
+  type: "spring",
+  damping: 20,
+  stiffness: 300,
+};
+
 export function Home() {
   const [items, setItems] = useState(ITEMS);
   const reverse = () => setItems((prev) => [...prev].reverse());
@@ -16,17 +22,24 @@ export function Home() {
     }),
     hidden: { opacity: 0 },
   };
-  // const x = useMotionValue(0);
-  // const opacity = useTransform(x, [-200, 0, 200], [0, 1, 0]);
+
   return (
     <div style={{ background: "pink" }}>
       this is Top
       <button onClick={reverse}>reverse</button>
-      {items.map((item) => (
-        <motion.div key={item} animate="visible" variants={variants} layout>
-          {item}
-        </motion.div>
-      ))}
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        {items.map((item) => (
+          <motion.div
+            key={item}
+            animate="visible"
+            variants={variants}
+            layout
+            transition={spring}
+          >
+            {item}
+          </motion.div>
+        ))}
+      </div>
     </div>
   );
 }
