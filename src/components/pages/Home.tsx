0c58c03ad66aef7b67dcf4e13413df35ac94d3e2
snamiki1212 <snamiki1212@gmail.com>
2021-03-23 12:19:50 -0700

    Add example

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,29 +1,32 @@
+import { useState } from "react";
 import { motion, useMotionValue, useTransform } from "framer-motion";
 
+const ITEMS = Array.from({ length: 10 }, (_, idx) => idx);
+
 export function Home() {
+  const [items, setItems] = useState(ITEMS);
+  const reverse = () => setItems((prev) => [...prev].reverse());
+
+  const variants = {
+    visible: (idx) => ({
+      opacity: 1,
+      transition: {
+        transform: `translate3d(${idx * 5}px, 0, 0)`,
+      },
+    }),
+    hidden: { opacity: 0 },
+  };
   // const x = useMotionValue(0);
   // const opacity = useTransform(x, [-200, 0, 200], [0, 1, 0]);
   return (
     <div style={{ background: "pink" }}>
       this is Top
-      <motion.div
-        drag="x"
-        animate={{
-          scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-        }}
-        transition={{
-          duration: 2,
-          ease: "easeInOut",
-          times: [0, 0.2, 0.5, 0.8, 1],
-          loop: Infinity,
-          repeatDelay: 1,
-        }}
-        style={{ width: "100px", height: "100px", background: "red" }}
-      >
-        OK
-      </motion.div>
+      <button onClick={reverse}>reverse</button>
+      {items.map((item) => (
+        <motion.div key={item} animate="visible" variants={variants} layout>
+          {item}
+        </motion.div>
+      ))}
     </div>
   );
 }
